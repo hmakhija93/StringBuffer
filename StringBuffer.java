176825b2c554ff5249881fbd843609e5ce63d596
Himanshu Makhija <himanshumakhija01@gmail.com>
2019-05-13 18:28:44 -0700

    Completed skeleton of StringBuffer.java.

--- StringBuffer.java
+++ StringBuffer.java
@@ -25,19 +25,170 @@ public final class StringBuffer extends Object {
 	}
 
 	// **** PRIVATE STRINGBUFFER METHODS ****
-	private boolean increaseCapacity() {
+	private boolean doubleCapacity() {
+		// Check if length overflows
+		if (buffer.length * 2 <= buffer.length) return false;
+		
 		char[] temp = new char[buffer.length * 2];
-		if (temp.length <= buffer.length) return false;
 		buffer = temp;
-		return true;
 	}
 
 	// **** PUBLIC STRINGBUFFER METHODS ****
 	public StringBuffer append(boolean b) {
-		if (b) {
-			
-		} else {
+	}
 
-		}
+	public StringBuffer append(char c) {
+	}
+
+	public StringBuffer append(char[] cStr) {
+
+	}
+
+	public StringBuffer append(char[] cStr, int offset, int len) {
+
+	}
+
+	public StringBuffer append(double d) {
+
+	}
+
+	public StringBuffer append(float f) {
+
+	}
+
+	public StringBuffer append(int i) {
+
+	}
+
+	public StringBuffer append(long l) {
+
+	}
+
+	public StringBuffer append(Object obj) {
+
+	}
+
+	public StringBuffer append(String str) {
+
+	}
+
+	public StringBuffer append(StringBuffer sb) {
+
+	}
+
+	public int capacity() {
+
+	}
+
+	public char charAt(int ind) {
+
+	}
+
+	public StringBuffer delete(int start, int end) {
+
+	}
+
+	public StringBuffer deleteCharAt(int ind) {
+
+	}
+
+	public void ensureCapacity(int minCap) {
+
+	}
+
+	public void getChars(int srcBegin, int srcEnd, char[] dst, int dstBegin) {
+
+	}
+
+	public int indexOf(String str) {
+
+	}
+
+	public int indexOf(String str, int fromIndex) {
+
+	}
+
+	public StringBuffer insert(int offset, boolean b) {
+
+	}
+
+	public StringBuffer insert(int offset, char c) {
+
+	}
+
+	public StringBuffer insert(int offset, char[] cStr) {
+
+	}
+
+	public StringBuffer insert(int ind, char[] cStr, int offset, int len) {
+
+	}
+
+	public StringBuffer insert(int offset, double d) {
+
+	}
+
+	public StringBuffer insert(int offset, float f) {
+	
+	}
+
+	public StringBuffer insert(int offset, int i) {
+
+	}
+
+	public StringBuffer insert(int offset, long l) {
+
+	}
+
+	public StringBuffer insert(int offset, Object obj) {
+
+	}
+
+	public StringBuffer insert(int offset, String str) {
+	
+	}
+
+	public int lastIndexOf(String str) {
+
+	}
+	
+	public int lastIndexOf(String str, int fromIndex) {
+
+	}
+
+	public int length() {
+	
+	}
+
+	public StringBuffer replace(int start, int end, String str) {
+
+	}
+
+	public StringBuffer reverse() {
+
+	}
+
+	public void setCharAt(int ind, char c) {
+
+	}
+
+	public void setLength(int newLength) {
+
+	}
+
+	public String subString(int start) {
+
+	}
+
+	public String subString(int start, int end) {
+
+	}
+
+	public String toString() {
+
+	}
+
+	public void trimToSize() {
+	
 	}
 }
